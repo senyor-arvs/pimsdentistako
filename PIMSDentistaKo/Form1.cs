934965d8635b5a0fc22a8efd87caeee9f6ea3637
kristiansantos11 <kristian.santos2000@gmail.com>
2021-06-17 19:03:30 +0800

    Changed licensing.

--- PIMSDentistaKo/Form1.cs
+++ PIMSDentistaKo/Form1.cs
@@ -16,5 +16,10 @@ namespace PIMSDentistaKo
         {
             InitializeComponent();
         }
+
+        private void bunifuDatePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
