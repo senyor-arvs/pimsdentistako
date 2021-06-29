8fc18de2fe83a5e80b788bfad2c5ed74f0001b83
kristiansantos11 <kristian.santos2000@gmail.com>
2021-06-29 19:26:47 +0800

    Made transaction view. Added custom calendar usercontrol component (its up to u to style it)

---
+++ pimsdentistako/Components/CustomCalendar.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pimsdentistako.Components
+{
+    /// <summary>
+    /// Interaction logic for CustomCalendar.xaml
+    /// </summary>
+    public partial class CustomCalendar : UserControl
+    {
+        public CustomCalendar()
+        {
+            InitializeComponent();
+        }
+
+        private void CustomCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
--- pimsdentistako/MainWindow.xaml.cs
+++ pimsdentistako/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using pimsdentistako.ViewModels;
 
 namespace pimsdentistako
 {
@@ -20,9 +21,32 @@ namespace pimsdentistako
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool pressed = false;
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new DashboardViewModel();
+        }
+
+        private void ProfilePicture_LeftClick(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Hello!");
+        }
+
+        private void transactionsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!this.pressed)
+            {
+                DataContext = new TransactionViewModel();
+                transactionsBtn.Background = Brushes.Gray;
+                this.pressed = true;
+            } else
+            {
+                DataContext = new DashboardViewModel();
+                transactionsBtn.Background = new SolidColorBrush(Color.FromRgb(189, 126, 74));
+                this.pressed = false;
+            }
+            
         }
     }
 }
