d7025ef141be3cde330188f524ca9f7a67608498
Emman <alcorizaem18@gmail.com>
2021-07-10 14:37:42 +0800

    try

--- pimsdentistako/obj/Debug/netcoreapp3.1/Views/TransactionView.g.i.cs
+++ pimsdentistako/obj/Debug/netcoreapp3.1/Views/TransactionView.g.i.cs
@@ -47,13 +47,13 @@ namespace pimsdentistako.Views {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/pimsdentistako;V1.0.0.0;component/views/transactionview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/pimsdentistako;component/views/transactionview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\TransactionView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -63,7 +63,7 @@ namespace pimsdentistako.Views {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
