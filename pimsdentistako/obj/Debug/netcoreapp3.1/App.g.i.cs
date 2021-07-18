f6ea82c97d6f75f5dadf4d56f2fa7536e099470f
kristiansantos11 <kristian.santos2000@gmail.com>
2021-07-18 14:57:31 +0800

    Merge part 2

--- pimsdentistako/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ pimsdentistako/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -47,7 +47,7 @@ namespace pimsdentistako {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace pimsdentistako {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
         public static void Main() {
             pimsdentistako.App app = new pimsdentistako.App();
             app.InitializeComponent();
