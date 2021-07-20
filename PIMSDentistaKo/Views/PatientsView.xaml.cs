b4732c2d0d6db938230691cfa2a30f2f72c28bff
xenz25 <86049869+xenz25@users.noreply.github.com>
2021-07-21 01:17:12 +0800

    change in csproj

--- PIMSDentistaKo/Views/PatientsView.xaml.cs
+++ PIMSDentistaKo/Views/PatientsView.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using pimsdentistako.Views;
 
+using pimsdentistako.DBHelpers;
+using pimsdentistako.DBElements;
+
 namespace pimsdentistako.Views
 {
     /// <summary>
@@ -22,21 +25,37 @@ namespace pimsdentistako.Views
         public PatientsView()
         {
             InitializeComponent();
+            DatabaseHelper.Init();
+            PatientHelper.MyDataGrid = patientDataGrid;
+            PatientHelper.TextCount = txtTotalPatient; //attach the count textblock to auto update whenever the list changes
+            PatientHelper.MyComboBox = searchByComboBox;
+            PatientHelper.InitList();
         }
 
         private void patientDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            PatientHelper.ListenToDataGrid();
+            PatientHelper.DisplaySelected(Name,MiddleName,LastName,Suffix,Nickname,Sex,CivilStatus,Address,Email,MobileNumber,HomeNumber,DateOfBirth,RefferedBy,Occupation,Company,OfficeNumber,FaxNumber,Age);
         }
         private void Master_File_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         private void dentalRecordBtn_Click(object sender, RoutedEventArgs e)
         {
             DentalRecordsView dentalRecordsView = new DentalRecordsView();
             dentalRecordsView.Show();
+        }
+
+        private void searchButton_Click(object sender, RoutedEventArgs e)
+        {
+            PatientHelper.ListenToSearch(txtBoxSearch);
+        }
+
+        private void clearSearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            PatientHelper.ListenToClearSearch(txtBoxSearch);
         }
     }
 }
